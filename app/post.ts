236c8bebb4db046b8b9075694b4e188312a901f1
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2021-11-23 15:54:09 +0900

    Dynamic Route Params

--- app/post.ts
+++ app/post.ts
@@ -2,6 +2,7 @@ import path from "path";
 import fs from "fs/promises";
 import parseFrontMatter from "front-matter";
 import invariant from "tiny-invariant";
+import { processMarkdown } from "@ryanflorence/md";
 
 export type Post = {
   slug: string;
@@ -20,27 +21,33 @@ function isValidPostAttributes(
   return attributes?.title;
 }
 
+export async function getPost(slug: string) {
+  let filepath = path.join(postsPath, slug + ".md");
+  let file = await fs.readFile(filepath);
+  let { attributes, body } = parseFrontMatter(file.toString());
+  let html = await processMarkdown(body);
+
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`
+  );
+
+  return { slug, html, title: attributes.title };
+}
+
 export async function getPosts() {
   let dir = await fs.readdir(postsPath);
 
   return Promise.all(
-    dir.map(async filename => {
-      let file = await fs.readFile(
-        path.join(postsPath, filename)
-      );
+    dir.map(async (filename) => {
+      let file = await fs.readFile(path.join(postsPath, filename));
+      let { attributes } = parseFrontMatter(file.toString());
 
-      let { attributes } = parseFrontMatter(
-        file.toString()
-      );
-
-      invariant(
-        isValidPostAttributes(attributes),
-        `${filename} has bad meta data!`
-      );
+      invariant(isValidPostAttributes(attributes));
 
       return {
         slug: filename.replace(/\.md$/, ""),
-        title: attributes.title
+        title: attributes.title,
       };
     })
   );
